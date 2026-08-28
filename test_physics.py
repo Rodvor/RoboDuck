74108bab84681bf6405ca2e528d71f0244f9b153
Milkkissss <eemeli.ruoranen@gmail.com>
2026-08-28 20:19:09 +0300

    Added test_physics

---
+++ test_physics.py
@@ -0,0 +1,34 @@
+import time
+import numpy as np
+from env.microduck_env import MicroDuckEnv
+
+env = MicroDuckEnv(render_mode="human")
+
+obs, info = env.reset()
+
+print("Starting physics test...")
+print("Initial height:", env.data.qpos[2])
+
+for i in range(500):
+
+    # ZERO MOTOR COMMAND
+    action = np.zeros(14, dtype=np.float32)
+
+    obs, reward, terminated, truncated, info = env.step(action)
+
+    env.render()
+
+    if i % 20 == 0:
+        print(
+            f"step={i:3d} "
+            f"height={env.data.qpos[2]:.4f} "
+            f"vel_z={env.data.qvel[2]:.4f}"
+        )
+
+    time.sleep(0.01)
+
+    if terminated or truncated:
+        print("DUCK FELL")
+        break
+
+env.close()
